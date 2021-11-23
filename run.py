0bda4524deec22dca59ac096af31e1cc1779bdca
pattytonyoneill <pattytonyoneill@gmail.com>
2021-11-23 17:38:59 +0000

    Get valid words and start of hangman

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import words
+
+"""
+Get a valid word
+"""
+def get_valid_word(words):
+    """ 
+    Randomly choose a word
+    """
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word.uppercase()
+
+def hangman():
+    word=get_valid_word(words)
+    """ 
+    letters in the word
+    """
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    """ 
+    letters guessed by user
+    """
+    guessed_letters = set()  
+    
