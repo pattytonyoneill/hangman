fe2ce2ab64be37edba32bc203ffa2787e78e8fd4
pattytonyoneill <pattytonyoneill@gmail.com>
2021-11-26 10:26:23 +0000

    Add hangman visual representation

--- run.py
+++ run.py
@@ -2,30 +2,47 @@ import random
 from words import words
 import string
 
-"""
-Get a valid word
-"""
+
 def get_valid_word(words):
-    """ 
-    Randomly choose a word
-    """
     word = random.choice(words)
     while '-' in word or ' ' in word:
         word = random.choice(words)
+
     return word.uppercase()
 
+
 def hangman():
-    word=get_valid_word(words)
-    """ 
-    letters in the word
-    """
+    word = get_valid_word(words)
+    """letters in the word"""
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
-    """ 
-    letters guessed by user
-    """
-    guessed_letters = set()  
-
-user_input = input('Please enter your guess: ')
-print(user_input)
+    """letters guessed by user"""
+    used_letters = set()  
+    lives = 7
     
+    """getting user input"""
+    while len(word_letters) > 0 and lives > 0:
+        """ Tell user  the lives left and letter that were used"""
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+                print('')
+
+            else:
+                lives = lives - 1
+                print('\nYour letter,', user_letter, 'is not in the word.')
+
+        elif user_letter in used_letters:
+            print('\nYou have already used that letter. Guess another letter.')
+
+        else:
+            print('\nThat is not a valid letter.')
+
+        hangman()
