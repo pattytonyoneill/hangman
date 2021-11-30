9fd483815fac71dd81162ed983cc3ada8b063124
pattytonyoneill <pattytonyoneill@gmail.com>
2021-11-30 11:36:48 +0000

    Edit of comments throughout run.py, Add play again section for playabilty,Add clear function to make the game easier to use, Add import os

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
+import os
 import random
 from words import words
 from hangman_visual import lives_visual_dict
 import string
+from os import system, name
+
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 def get_valid_word(words):
@@ -13,11 +19,14 @@ def get_valid_word(words):
 
 
 def hangman():
+    """
+    The hangman game
+    """
     word = get_valid_word(words)
-    """letters in the word"""
+    # letters in the word
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
-    """letters guessed by user"""
+    # letters guessed by user
     used_letters = set()
     lives = 7
     print('Welcome to Hangman!')
@@ -26,17 +35,22 @@ def hangman():
     print('Good Luck!')
     print(' ')
 
-    """getting user input"""
+    # getting user input
     while len(word_letters) > 0 and lives > 0:
-        """Tell user the lives lefft and the letters that were used"""
-        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))  # noqa
+        # Tell user the lives lefft and the letters that were used
+        clear()
+        print(
+            'You have', lives, 'lives left and you have used these letters: ',
+            ' '.join(used_letters))
 
-        """Current word"""
-        word_list = [letter if letter in used_letters else '-' for letter in word]  # noqa
+        # Current word
+        word_list = [
+            letter if letter in used_letters else '-' for letter in word]
         print(lives_visual_dict[lives])
         print('Current word: ', ' '.join(word_list))
 
-        """User Guesses"""
+        # User Guesses
+        print(word)
         user_letter = input('Guess a letter: ').upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
@@ -45,22 +59,37 @@ def hangman():
                 print('')
 
             else:
-                """Removes a life if wrong"""
+                # Removes a life if wrong
                 lives = lives - 1
-                print('\nSorry, your letter,', user_letter, 'is not in the word.')  # noqa
+                print(
+                    '\nSorry, your letter,', user_letter,
+                    'is not in the word.')
 
         elif user_letter in used_letters:
-            print('\nSorry, you have already used that letter. Please guess another letter.')  # noqa
+            print(
+                '\nSorry, you have already used that letter. '
+                'Please guess another letter.')
 
         else:
+            clear()
             print('\nSorry, that is not a valid letter.')
 
-    """ Get here when len(word_letters) == 0 OR when lives == 0"""
+    # Get here when len(word_letters) == 0 OR when lives == 0
     if lives == 0:
         print(lives_visual_dict[lives])
         print('You died, sorry. The word was', word)
     else:
         print('Yahoo! You guessed the word', word, '!!')
+    play_again()
+
+
+def play_again():
+    user_play_again = input('Do you want to play again? (Y/N) ').upper()
+    if user_play_again == 'Y':
+        hangman()
+    else:
+        clear()
+        print('Thanks for playing hangman!')
 
 
 if __name__ == '__main__':
