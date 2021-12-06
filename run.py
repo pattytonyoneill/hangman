7df688d09b6e894b0ae5167601c56284c89acbbf
pattytonyoneill <pattytonyoneill@gmail.com>
2021-12-06 12:38:35 +0000

    Add update game function

--- run.py
+++ run.py
@@ -85,7 +85,10 @@ def hangman():
             clear()
             print('\nSorry, that is not a valid letter.  Please guess again!')
 
-    # Here when len(word_letters) == 0 OR when lives == 0
+
+def update_game():
+    global lives
+# Here when len(word_letters) == 0 OR when lives == 0
     if lives == 0:
         print(lives_dict[lives])
         print('You died, sorry. The correct word was', word)
@@ -94,6 +97,7 @@ def hangman():
         print('Yahoo! You guessed correctly', word, '!!')
         print("\U0001f44D")
     play_again()
+
 
 
 def play_again():
